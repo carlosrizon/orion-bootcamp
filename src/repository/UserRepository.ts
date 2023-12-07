1546df83e29d78b9fc38f8ae16c5d0f9d7cbcece
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-12-07 18:11:06 -0300

    docs: adiciona documentação à UserRepository

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,17 +1,31 @@
 import { MysqlDataSource } from '../config/database';
 import * as bcrypt from 'bcrypt';
 import User from '../entity/User';
+import { DeepPartial } from 'typeorm';
 
+/**
+ * Classe que implementa operações de criação e manipulação de usuários no database {@link User}
+ */
 export class UserRepository {
   private userRepository = MysqlDataSource.getRepository(User);
 
+  /**
+   * @constructor
+   * Cria instância da classe UserRepository
+   */
   constructor() {
     this.userRepository = MysqlDataSource.getRepository(User);
   }
 
   private _saltRounds = (): number => 10;
 
-  async createAndSave(userData): Promise<User> {
+  /**
+   * Função de criação de novos unuários no database {@link User}
+   * @async
+   * @param userData - objeto que contém dados para criação de susuário
+   * @returns - retorna promise User a ser resolvida quando da criação do usuário no banco de dados ou rejeitada caso não seja possível criá-lo no banco
+   */
+  async createAndSave(userData: DeepPartial<User>): Promise<User> {
     const hashpassword = await bcrypt.hash(
       userData.password,
       this._saltRounds()
@@ -29,6 +43,13 @@ export class UserRepository {
     return newUser;
   }
 
+  /**
+   * Função que implementa busca no database {@link User} e retorna instância de usuário a partir da propriedade email ou ID do usuário
+   * @async
+   * @param {string | number} value - valor conhecido da propriedade indicada para busca do usuário
+   * @param {string} property - indicação da propriedade (ID ou e-mail) a ser utilizada na busca
+   * @returns - Promise User a ser resolvida quando encontrado o usuário ou rejeitada caso este não exista
+   */
   async findUserByEmailOrID(
     value: string | number,
     property: string
