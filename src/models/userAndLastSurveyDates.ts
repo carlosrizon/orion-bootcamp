e6489c30a9eb88651fb6d92382476ac014cfe9cb
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-12-07 18:02:52 -0300

    fix: faz pequenas correções de documentação e importação

--- src/models/userAndLastSurveyDates.ts
+++ src/models/userAndLastSurveyDates.ts
@@ -1,6 +1,6 @@
 /**
  * @interface
- * Modelo de objeto contendo data de criação do usuário e data de reaçização da última pesquisa de satisfação associada
+ * Modelo de objeto contendo data de criação do usuário e data de realização da última pesquisa de satisfação associada
  */
 export default interface UserAndLastSurveyDates {
   latestSurvey: Date;
