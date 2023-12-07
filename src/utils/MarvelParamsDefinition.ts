b089cad45700c4969dad95a086577dbb857c7858
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-12-07 17:47:26 -0300

    feat(MarvelParamsDefinition): acrescenta construtor à classe e modifica métodos

--- src/utils/MarvelParamsDefinition.ts
+++ src/utils/MarvelParamsDefinition.ts
@@ -1,4 +1,4 @@
-import { format, subDays } from 'date-fns';
+import MarvelParamsModel from '../models/MarvelParamsModel';
 import md5 from 'md5';
 import 'dotenv/config';
 
@@ -6,12 +6,84 @@ import 'dotenv/config';
  * Classe auxiliar que fornece valores constantes a serem utilizados como parâmetros para requisição de dados à API da Marvel
  */
 export default class MarvelParamsDefinition {
+  private urlComplement: string;
+  private hash: string;
+  private apikey: string;
+  private ts: number;
+  private limit: number;
+
   /**
-   * Função geradora que retorna valores a serem utilizados como offset na requisição.
-   * Primeiro valor gerado é \'0' e demais \'100', considerando limite máximo de retorno de dados da API
+   * @constructor
+   * Cria instância da classe MarvelParamsDefinition
+   * @param {string} urlComplement - alias string da categoria utilizada como complmento da url para requisição à API Marvel
    */
-  *offsetter() {
-    let value = 0;
+  constructor(urlComplement: string) {
+    this.urlComplement = urlComplement;
+    this.ts = this._getTimestamp();
+    this.hash = this._hashGenerator(this.ts);
+    this.apikey = this._apikey();
+    this.limit = this.maxMarvelAPILimit();
+  }
+
+  /**
+   * @private
+   * Método que retorna valor numérico correspondente ao número de milissegundos desde 1º de janeiro de 1970, representando timestamp de data e hora atuais
+   * @returns {number}  - valor numérico que representa data e hora atuais em milissegundos
+   */
+  private _getTimestamp(): number {
+    return Date.now();
+  }
+
+  /**
+   * @private
+   * Método que retorna chave pública de cadastro na API a ser utilizada na requisição
+   * @returns {string} - chave pública Marvel
+   */
+  private _apikey(): string {
+    return process.env.MARVEL_API_KEY;
+  }
+
+  /**
+   * @private
+   * Método gerador de hash de autorização a ser utilizada para realização das requisições à API da Marvel
+   * @param {number} timestamp - valor numérico que representa data e hora atuais em milissegundos. Valor único para composição da hash
+   * @returns {string} - string correspondente à criptografia da união de timestamp, chave pública e chave privada da API, considerando algoritmo md5
+   */
+  private _hashGenerator(timestamp: number): string {
+    return md5(
+      timestamp + process.env.MARVEL_PRIVATE_KEY + process.env.MARVEL_API_KEY
+    );
+  }
+
+  /**
+   * Método get de valores de parâmetros a serem utilizados para requisição à API da Marvel
+   * @returns {MarvelParamsModel} - retorna objeto do tipo {@link MarvelParamsModel}
+   */
+  getParams(): MarvelParamsModel {
+    return {
+      ts: this.ts,
+      hash: this.hash,
+      apikey: this.apikey,
+      limit: this.limit
+    };
+  }
+
+  /**
+   * Método que concatena urlBase e endpoint da requisição
+   * @returns {string} - string correspondente à concatenação da urlBase e endpoint da requisição
+   */
+  getUrl(): string {
+    const baseURL = process.env.MARVEL_URL_BASE;
+    return `${baseURL}/${this.urlComplement}`;
+  }
+
+  /**
+   * Função geradora que retorna valores a serem utilizados como offset na requisição a partir de valor de offset inicial fornecido.
+   * Caso não seja fornecido valor inicial para o offset, adota-se zero (0).
+   * @param {number} offsetStart - número correspondete ao offset inicial à ser aplicado na requisição
+   */
+  *offsetter(offsetStart: number = 0): Generator<number, void> {
+    let value = offsetStart;
     while (true) {
       yield value;
       value += this.maxMarvelAPILimit();
@@ -19,63 +91,11 @@ export default class MarvelParamsDefinition {
   }
 
   /**
-   * Método que retorna valor numérico correspondente ao número de milissegundos desde 1º de janeiro de 1970, representando timestamp de data e hora atuais
-   * @returns {number}  - valor numérico que representa data e hora atuais em milissegundos
-   */
-  getTimestamp(): number {
-    return Date.now();
-  }
-
-  /**
-   * Método que retorna URL Base da plataforma a ser utilizada na requisição
-   * @returns {string} - URL Base Marvel Requisições GET
-   */
-  baseURL(): string {
-    return process.env.MARVEL_URL_BASE;
-  }
-
-  /**
-   * Método que retorna chave pública de cadastro na API a ser utilizada na requisição
-   * @returns {string} - chave pública Marvel
-   */
-  apikey(): string {
-    return process.env.MARVEL_API_KEY;
-  }
-
-  /**
    * Método que retorna valor máximo de dados a serem retornados pela API Marvel a cada requisição.
    * Valor definido como sendo o máximo permitido pela própria API
-   *
    * @returns {number} - número máximo de dados retornados por requisição
    */
   maxMarvelAPILimit(): number {
     return 100;
-  }
-
-  /**
-   * Método que retorna string em formato de data correspondente à data base a partir da qual os dados modificados devem ser retornados pela API da Marvel
-   *
-   * @param {number} daysInterval - número de dias a ser subtraído da data atual para cálculo da data base
-   * @returns {strig} - string em formato de data correspondete à data base de atualização dos dados a serem retornados
-   */
-  modifiedsSince(daysInterval: number): string {
-    const today = new Date();
-    const daysAgoString: string = format(
-      subDays(today, daysInterval),
-      'yyyy-MM-dd'
-    );
-    return daysAgoString;
-  }
-
-  /**
-   * Método gerador de hash de autorização a ser utilizada para realização das requisições à API da Marvel
-   *
-   * @param {number} timestamp - valor numérico que representa data e hora atuais em milissegundos
-   * @returns {string} - string correspondente à criptografia da união de timestamp, chave pública e chave privada da API, considerando algoritmo md5
-   */
-  hashGenerator(timestamp: number): string {
-    return md5(
-      timestamp + process.env.MARVEL_PRIVATE_KEY + process.env.MARVEL_API_KEY
-    );
   }
 }
