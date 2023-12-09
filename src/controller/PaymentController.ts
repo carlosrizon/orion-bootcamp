a8bd023dc52df6176fef463de0df9798407f3aa4
Nickolas Luan <Nickolasluan15@gmail.com>
2023-12-09 08:09:19 -0300

    commit

--- src/controller/PaymentController.ts
+++ src/controller/PaymentController.ts
@@ -3,51 +3,108 @@ import { MercadoPagoConfig, Payment } from 'mercadopago';
 import 'dotenv/config';
 import { randomUUID } from 'crypto';
 import { MysqlDataSource } from '../config/database';
-import { DeepPartial } from 'typeorm';
 import User from '../entity/User';
-import Payments from '../entity/payment';
-import QRCode from 'qrcode';
+import Payments from '../entity/Payment';
 import { EmailSender } from '../library/mail';
 
 const mercadoPagoToken = process.env.TOKEN_MERCADOPAGO;
+const NOTIFICATION_WEB = process.env.NOTIFICATION_WEB;
 const client = new MercadoPagoConfig({ accessToken: mercadoPagoToken });
 const payment = new Payment(client);
 
 export class PaymentController {
+  /**
+   * @swagger
+   *
+   * /v1/paymentnotifications:
+   *   post:
+   *     summary: recebe notificações do gateway de pagamento
+   *     description: recebe notificações do gateway de pagamento com atualizações
+   *     tags: [Payment Notifications]
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       '200':
+   *           description: 'Requisição bem sucedida.'
+   *           content:
+   *             application/json:
+   *               schema:
+   *                 type: object
+   *               properties:
+   *                 date:
+   *                   type: date
+   *                   description: Data de envio da resposta à requisição
+   *                 status:
+   *                   type: boolean
+   *                   description: Status da efetivação do pagamento
+   *                 data:
+   *                   type: string
+   *                   description: Mensagem de sucesso
+   *               example:
+   *                 date: 2023-10-28T19:59:19.751Z
+   *                 status: true
+   *                 data: Pagamento aprovado com sucesso.
+   *
+   *       '500':
+   *           description: 'Erro interno.'
+   *           content:
+   *             application/json:
+   *               schema:
+   *                 type: object
+   *                 properties:
+   *                 date:
+   *                   type: date
+   *                   description: Data de envio da resposta à requisição
+   *                 status:
+   *                   type: boolean
+   *                   description: Status da efetivação do pagamento
+   *                 data:
+   *                   type: string
+   *                   description: Mensagem de erro
+   *               example:
+   *                 date: 2023-10-28T19:59:19.751Z
+   *                 status: true
+   *                 data: 'Ocorreu um erro interno no servidor.'
+   */
   async reciveNotificationPayment(req: Request, res: Response) {
-    const userRepository = await MysqlDataSource.getRepository(User);
-    const paymentRepository = await MysqlDataSource.getRepository(Payments);
+    const paymentRepository = MysqlDataSource.getRepository(Payments);
     const notification = req.body;
     console.log(notification);
-
+    const paymentId = notification?.data?.id;
     if (
       notification.action === 'payment.created' &&
       notification.type === 'payment'
     ) {
+      console.log('entrou created', notification);
       return res.status(201).send({
         date: new Date(),
         status: true,
         data: 'Pagamento foi criado.'
       });
-    }
-    if (
+    } else if (
       notification.action === 'payment.updated' &&
-      notification.type === 'payment'
+      notification.type === 'payment' &&
+      paymentId
     ) {
-      const paymentId = notification.data.id;
       const dataPayment = await payment.get({
         id: paymentId
       });
+
       const statusPayment = await dataPayment.status;
-      console.log(statusPayment); // Mostrar status do pagamento no terminal
-      if (statusPayment === 'approved') {
+
+      if (paymentId && statusPayment === 'approved') {
+        console.log('Entrou aprovado', notification);
         const paymentRecord = await paymentRepository.findOneBy({
-          paymentId: paymentId
+          paymentId: Number(paymentId)
         });
         if (paymentRecord) {
           paymentRecord.status = 'approved';
           await MysqlDataSource.manager.save(paymentRecord);
         }
+        if (paymentRecord?.user?.email) {
+          new EmailSender().sendPaymentConfirmationEmail(paymentRecord.user);
+        }
+
         return res.status(200).send({
           date: new Date(),
           status: true,
@@ -57,6 +114,15 @@ export class PaymentController {
       console.log(dataPayment);
     }
 
+    if (paymentId) {
+      const paymentRecord = await paymentRepository.findOneBy({
+        paymentId: Number(paymentId)
+      });
+      if (paymentRecord?.user?.email) {
+        new EmailSender().sendPaymentFailureEmail(paymentRecord.user);
+      }
+    }
+
     return res.status(500).send({
       date: new Date(),
       status: false,
@@ -64,15 +130,72 @@ export class PaymentController {
     });
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/createpayment:
+   *   post:
+   *     summary: cria novo pagamento para o usuário
+   *     description: cria novo pagamento para o usuário e envia e-mail com link para pagamento via pix da compra
+   *     security:
+   *       - BearerAuth: []
+   *     tags: [Create Payment]
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       '201':
+   *           description: 'Requisição bem sucedida.'
+   *           content:
+   *             application/json:
+   *               schema:
+   *                 type: object
+   *               properties:
+   *                 date:
+   *                   type: date
+   *                   description: Data de envio da resposta à requisição
+   *                 status:
+   *                   type: boolean
+   *                   description: Status da criação do pagamento
+   *                 data:
+   *                   type: number
+   *                   description: PaymentId do pagamento criado
+   *               example:
+   *                 date: 2023-10-28T19:59:19.751Z
+   *                 status: true
+   *                 data: 68434960766
+   *
+   *       '500':
+   *           description: 'Erro interno.'
+   *           content:
+   *             application/json:
+   *               schema:
+   *                 type: object
+   *                 properties:
+   *                 date:
+   *                   type: date
+   *                   description: Data de envio da resposta à requisição
+   *                 status:
+   *                   type: boolean
+   *                   description: Status da criação do pagamento
+   *                 data:
+   *                   type: string
+   *                   description: Mensagem de erro
+   *               example:
+   *                 date: 2023-10-28T19:59:19.751Z
+   *                 status: true
+   *                 data: 'Erro ao criar pagamento.'
+   *
+   */
   async createPayment(req: Request, res: Response) {
     try {
-      const user_email = req.body.email;
+      const user_id = req.body.user.id;
       const paymentRepository = await MysqlDataSource.getRepository(Payments);
       const userRepository = await MysqlDataSource.getRepository(User);
+      const user = await userRepository.findOneBy({ id: user_id });
       const notification_url =
-        'https://ba17-177-188-46-238.ngrok-free.app/v1/paymentnotifications'; //Endpoint onde o webooks envia notificações do pagamento.
+        'https://3e6a-177-188-46-238.ngrok-free.app/v1/paymentnotifications';
       const description = 'Marvelpedia - Adquirir um pôster exclusivo!';
-      const idempotencyKey = randomUUID(); //Key única aleatória para identificação de cada pedido.
+      const idempotencyKey = randomUUID();
 
       const result = await payment.create({
         body: {
@@ -80,21 +203,20 @@ export class PaymentController {
           description: description,
           payment_method_id: 'pix',
           payer: {
-            email: req.body.email
+            email: user.email
           },
           notification_url: notification_url
         },
         requestOptions: { idempotencyKey: idempotencyKey }
       });
 
-      const user = await userRepository.findOneBy({ email: user_email });
       const paymentEntity = new Payments();
       paymentEntity.paymentId = result.id;
       paymentEntity.status = result.status;
       paymentEntity.user = user;
-      paymentEntity.email = user_email;
-      paymentEntity.qrcode =
-        result.point_of_interaction.transaction_data.qr_code;
+      (paymentEntity.email = user.email),
+        (paymentEntity.qrcode =
+          result.point_of_interaction.transaction_data.qr_code);
 
       await MysqlDataSource.manager.save(paymentEntity);
       new EmailSender().sendQrcodePayment(result.id, user);
